1bc6aaf652575cb464dcdc01c90903f6faeb352d
Full Name <theofficialwebsiteguys@gmail.com>
2025-08-27 11:44:20 -0400

    LOGIN FIX

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   apiUrl: 'https://dispensary-api-ac9613fa4c11.herokuapp.com/api',
   venueId: 'df759ba5e7360809',
   flower_power_api_key: '564ee591-42df-46d9-91c0-ff3507e316ec',
-  aeropay_api_key: 'cede8696-a122-4537-a94b-43dbdaf12abb',
-  aeropay_api_secret: 'd5bd2c13-206e-4595-afe6-8fc2e0839f9e',
+  // aeropay_api_key: 'cede8696-a122-4537-a94b-43dbdaf12abb',
+  // aeropay_api_secret: 'd5bd2c13-206e-4595-afe6-8fc2e0839f9e',
   aeropay_url: "https://api.aeropay.com",
-  aeropay_merchant_id: 1689,
+  // aeropay_merchant_id: 1689,
   db_api_key: 'caf0c918d083b815a16fe0546e3802de4423e9b33c26aba8e1fa2a614966978d',
   location_rochester: '0e443789-178c-45aa-aefb-e4f55fe46b84'
 };
